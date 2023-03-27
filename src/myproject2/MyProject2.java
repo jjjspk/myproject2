49b6118d2601291413a5882876fef01a48332f7f
Student <Student@5CD3019B0L.control.ad.sit.kmutt.ac.th>
2023-03-27 14:25:24 +0700

    1 command

---
+++ src/myproject2/MyProject2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myproject2;
+
+/**
+ *
+ * @author Student
+ */
+public class MyProject2 {
+
+  
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+             
+    }
+    
+}
